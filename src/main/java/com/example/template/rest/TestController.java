4349e6c451f32269f1786357673998cd90b0228e
truenemec <truenemec@mail.ru>
2021-09-16 22:40:28 +0300

    init

---
+++ src/main/java/com/example/template/rest/TestController.java
@@ -0,0 +1,14 @@
+package com.example.template.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+    @GetMapping
+    public String get(){
+        return "OK";
+    }
+}
